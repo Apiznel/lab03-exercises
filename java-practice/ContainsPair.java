217b35f4747795b3bd9ab44d43af6ea01e4e8799
Cornell Chin <cchin2@unca.edu>
2026-09-03 11:22:21 -0400

    Add ContainsPair.java from lab2 (accidentally copied from lab00-warmup)

--- java-practice/ContainsPair.java
+++ java-practice/ContainsPair.java
@@ -1,34 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class ContainsPair {
+
     public static void main(String[] args) {
-        // System.out.println(ContainsPair.containsPair1(List.of(3, 9, 5, 6, 6, 7)));
-        // System.out.println(ContainsPair.containsPair2(List.of(3, 9, 5, 6, 6, 7)));
-        // System.out.println(ContainsPair.containsPair3(List.of(3, 9, 5, 6, 10, 7)));
-        // System.out.println(ContainsPair.containsPair4(List.of(3, 9, 5, 6, 10, 7, 2,
-        // 3, 2)));
-        // System.out.println(ContainsPair.containsPair5(List.of(3, 9, 5, 6, 10, 7, 2, 3, 2)));
-        System.out.println(ContainsPair.containsPair6(List.of(3, 9, 5, 6, 10, 7, 2, 3, 2)));
+        checkCase(Arrays.asList(), false, "empty");
+        checkCase(Arrays.asList(3), false, "singleton");
+        checkCase(Arrays.asList(1, 2, 3, 2), true, "duplicate");
+        checkCase(Arrays.asList(5, 2, -10, 44, 90), false, "no duplicate");
+        checkCase(Arrays.asList(-2, 4, -2), true, "negative duplicate");
+        checkCase(Arrays.asList(7, 7, 7), true, "repeated more than twice");
     }
 
-    public static boolean containsPair1(List<Integer> l) {
-        for (int i = 0; i < l.size() - 1; i++) {
-            if (l.get(i).equals(l.get(i + 1))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean containsPair2(List<Integer> l) {
-        for (int i = 0; i < (l.size() - 1); i++) {
-            for (int j = i + 1; j < l.size(); j++) {
-                if (l.get(i).equals(l.get(j))) {
+    public static boolean checkNestedLoops(List<Integer> values) {
+        for (int i = 0; i < values.size() - 1; i++) {
+            for (int j = i + 1; j < values.size(); j++) {
+                if (values.get(i).equals(values.get(j))) {
                     return true;
                 }
             }
@@ -36,42 +25,20 @@ public class ContainsPair {
         return false;
     }
 
-    public static boolean containsPair3(List<Integer> l) {
-        for (int i = 0; i < (l.size() - 1); i++) {
-            for (int j = 0; j < l.size(); j++) {
-                if (l.get(i).equals(l.get(j))) {
-                    return true;
-                }
-            }
+    public static boolean checkWithSet(List<Integer> values) {
+        Set<Integer> s = new HashSet<Integer>(values);
+        return s.size() != values.size();
+    }
+
+    private static void checkCase(List<Integer> values, boolean expected, String label) {
+        boolean nestedResult = checkNestedLoops(values);
+        boolean setResult = checkWithSet(values);
+        if (nestedResult == expected && setResult == expected) {
+            System.out.println("PASS: " + label);
+        } else {
+            System.out.println(
+                    "FAIL: " + label + " expected " + expected
+                            + " but got nested=" + nestedResult + ", set=" + setResult);
         }
-        return false;
     }
-
-    public static boolean containsPair4(List<Integer> l) {
-        List<Integer> unique = new ArrayList<Integer>();
-        for (int i = 0; i < l.size(); i++) {
-            if (unique.contains(l.get(i))) {
-                return true;
-            }
-            unique.add(l.get(i));
-        }
-        return false;
-    }
-
-    public static boolean containsPair5(List<Integer> l) {
-        Map<Integer, Integer> s = new HashMap<Integer, Integer>();
-        for (int i = 0; i < l.size(); i++) {
-            if (s.containsKey(l.get(i))) {
-                return true;
-            }
-            s.put(l.get(i), 1);
-        }
-        return false;
-    }
-
-    public static boolean containsPair6(List<Integer> l) {
-        Set<Integer> s = new HashSet<Integer>(l);
-        return s.size() != l.size();
-    }
-
 }
